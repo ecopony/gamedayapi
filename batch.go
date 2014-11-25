b7678f8902746bccd18f11366551d3c74d535705
Ed Copony <ecopony@yahoo.com>
2014-11-24 19:38:09 -0800

    Rolling through the days on the batch fetch.

--- batch.go
+++ batch.go
@@ -6,15 +6,20 @@ import (
 	"strconv"
 )
 
-func FetchByYearAndTeam(year int, team string, fetchFunc FetchFunc) {
-	log.Println("Batchin it in " + strconv.Itoa(year) + " for " + team)
+func FetchByYearAndTeam(year int, teamCode string, fetchFunc FetchFunc) {
+	log.Println("Batchin it in " + strconv.Itoa(year) + " for " + teamCode)
 	openingDay, finalDay := OpeningAndFinalDatesForYear(year)
 	currentDay := openingDay
 
-	log.Println(openingDay)
-	log.Println(finalDay)
-	currentDay = currentDay.Add(time.Hour*24)
-	log.Println(currentDay)
+	for {
+		game := GameFor(teamCode, currentDay.Format("2006-01-02"))
+		log.Println(game.GameDataDirectory)
+
+		currentDay = currentDay.Add(time.Hour*24)
+		if currentDay.After(finalDay) {
+			break
+		}
+	}
 }
 
 type FetchFunc func(game *Game)
