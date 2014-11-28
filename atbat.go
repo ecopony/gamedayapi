9db7c6675c25ed0f11a65d823f0866edb9b059c3
Ed Copony <ecopony@yahoo.com>
2014-11-28 15:23:57 -0800

    Filling in the at bat attributes.

--- atbat.go
+++ atbat.go
@@ -3,6 +3,19 @@ package gamedayapi
 // AtBat represents each at bat in an inning, and will have all the pitches thrown.
 type AtBat struct {
 	Num string `xml:"num,attr"`
+	B string `xml:"b,attr"`
+	S string `xml:"s,attr"`
+	O string `xml:"o,attr"`
+	StartTFS string `xml:"start_tfs,attr"`
+	StartTFSZulu string `xml:"start_tfs_zulu,attr"`
+	Batter string `xml:"batter,attr"`
+	Stand string `xml:"stand,attr"`
+	BHeight string `xml:"b_height,attr"`
+	Pitcher string `xml:"pitcher,attr"`
+	PThrows string `xml:"p_throws,attr"`
+	Des string `xml:"des,attr"`
+	DesEs string `xml:"des_es,attr"`
+	Event string `xml:"event,attr"`
 
 	Pitches []Pitch `xml:"pitch"`
 }
