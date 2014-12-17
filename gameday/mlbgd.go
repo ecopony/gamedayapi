dfcb8c4a257df36c4d0e14a498b84262221aa8a8
Ed Copony <ecopony@yahoo.com>
2014-12-17 07:59:33 -0800

    Refactoring the command line bits to branch on a map -- moves the logic for the separate commands into their own functions.

--- gameday/mlbgd.go
+++ gameday/mlbgd.go
@@ -8,92 +8,33 @@ import (
 	"time"
 )
 
-var commands = []string{"game",
-	"games-for-team-and-year",
-	"games-for-team-and-years",
-	"games-for-year",
-	"help",
-	"valid-teams-for-year",
-}
+var commands = map[string]func([]string) {}
 
 func main() {
+	initializeCommands()
 	args := os.Args[1:]
 	command := args[0]
-	if !isCommandValid(command) {
+
+	if function, ok := commands[command]; ok {
+		function(args)
+	} else {
 		fmt.Println(fmt.Sprintf("%s is not a valid command. Valid commands:", command))
 		printValidCommands()
 		os.Exit(1)
 	}
+}
 
-	if command == "help" {
-		printValidCommands()
-		os.Exit(0)
-	}
-
-	if command == "game" {
-		validateArgLength(args, 2)
-		teamCode := args[1]
-		date, err := time.Parse("2006-01-02", args[2])
-		if err != nil {
-			fmt.Println("Date must be in the format 2006-01-02")
-			os.Exit(1)
-		}
-		game, err := gamedayapi.GameFor(teamCode, date)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		game.EagerLoad()
-		fmt.Println("Game files saved to " + gamedayapi.BaseCachePath() + game.GameDataDirectory)
-	} else if command == "games-for-year" {
-		validateArgLength(args, 1)
-		yearArg := args[1]
-		year, err := strconv.Atoi(yearArg)
-		if err != nil {
-			fmt.Println("Year is not valid")
-		}
-		teams := gamedayapi.TeamsForYear(year)
-		for _, team := range teams {
-			fmt.Println(team)
-			gamedayapi.FetchByTeamAndYear(team, year, eagerLoadGame) // No goroutines here yet.
-		}
-	} else if command == "valid-teams-for-year" {
-		validateArgLength(args, 1)
-		yearArg := args[1]
-		year, err := strconv.Atoi(yearArg)
-		if err != nil {
-			fmt.Println("Year is not valid")
-		}
-		teams := gamedayapi.TeamsForYear(year)
-		fmt.Println(teams)
-	} else {	// games-for-team-and-year or games-for-team-and-years
-		validateArgLength(args, 2)
-		teamCode := args[1]
-		yearArgs := args[2:]
-		var years []int
-		for i := 0; i < len(yearArgs); i++ {
-			year, err := strconv.Atoi(yearArgs[i])
-			if err != nil {
-				fmt.Println("Year is not valid")
-			}
-			years = append(years, year)
-		}
-		gamedayapi.FetchByTeamAndYears(teamCode, years, eagerLoadGame)
-	}
+func initializeCommands() {
+	commands["game"] = game
+	commands["games-for-team-and-year"] = gameForTeamAndYears
+	commands["games-for-year"] = gamesForYear
+	commands["help"] = help
+	commands["valid-teams-for-year"] = validTeamsForYear
 }
 
 func eagerLoadGame(game *gamedayapi.Game) {
 	game.EagerLoad()
 	fmt.Println("Game files saved to " + gamedayapi.BaseCachePath() + game.GameDataDirectory)
-}
-
-func isCommandValid(command string) bool {
-	for _, validCommand := range commands {
-		if command == validCommand {
-			return true
-		}
-	}
-	return false
 }
 
 func printUsage() {
@@ -103,7 +44,7 @@ func printUsage() {
 func printValidCommands() {
 	printUsage()
 	fmt.Println("Valid commands:")
-	for _, validCommand := range commands {
+	for validCommand, _ := range commands {
 		fmt.Println(fmt.Sprintf("\t%s", validCommand))
 	}
 }
@@ -113,4 +54,74 @@ func validateArgLength(args []string, validLength int) {
 		printUsage()
 		os.Exit(1)
 	}
+}
+
+func game(args []string) {
+	validateArgLength(args, 2)
+	teamCode := args[1]
+	date, err := time.Parse("2006-01-02", args[2])
+	if err != nil {
+		fmt.Println("Date must be in the format 2006-01-02")
+		os.Exit(1)
+	}
+	game, err := gamedayapi.GameFor(teamCode, date)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	game.EagerLoad()
+	fmt.Println("Game files saved to " + gamedayapi.BaseCachePath() + game.GameDataDirectory)
+}
+
+func gameForTeamAndYear(args []string) {
+	validateArgLength(args, 1)
+	yearArg := args[1]
+	year, err := strconv.Atoi(yearArg)
+	if err != nil {
+		fmt.Println("Year is not valid")
+	}
+	teams := gamedayapi.TeamsForYear(year)
+	for _, team := range teams {
+		fmt.Println(team)
+		gamedayapi.FetchByTeamAndYear(team, year, eagerLoadGame) // No goroutines here yet.
+	}
+}
+
+func gameForTeamAndYears(args []string) {
+	gamesFor(args)
+}
+
+func gamesForYear(args []string) {
+	gamesFor(args)
+}
+
+func gamesFor(args []string) {
+	validateArgLength(args, 2)
+	teamCode := args[1]
+	yearArgs := args[2:]
+	var years []int
+	for i := 0; i < len(yearArgs); i++ {
+		year, err := strconv.Atoi(yearArgs[i])
+		if err != nil {
+			fmt.Println("Year is not valid")
+		}
+		years = append(years, year)
+	}
+	gamedayapi.FetchByTeamAndYears(teamCode, years, eagerLoadGame)
+}
+
+func help(args []string) {
+	printValidCommands()
+	os.Exit(0)
+}
+
+func validTeamsForYear(args []string) {
+	validateArgLength(args, 1)
+	yearArg := args[1]
+	year, err := strconv.Atoi(yearArg)
+	if err != nil {
+		fmt.Println("Year is not valid")
+	}
+	teams := gamedayapi.TeamsForYear(year)
+	fmt.Println(teams)
 }
